cacf6873fda178e0372866dd597e0e951d1e19d5
dannikaml <dannikaml@hotmail.com>
2023-08-01 19:24:49 -0500

    added save button and new chapter button, working on css properties for both

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [chapters, setChapters] = useState([]);
+  const [currentChapter, setCurrentChapter] = useState('');
+
+  const handleSaveChapter = () => {
+    if (currentChapter.trim() !== '') {
+      setChapters((prevChapters) => [...prevChapters, currentChapter]);
+      setCurrentChapter('');
+    }
+  };
+
+  const handleChapterClick = (chapter) => {
+    setCurrentChapter(chapter);
+  };
+
+  const handleNewChapter = () => {
+    setCurrentChapter('');
+  };
+
   return (
     <div className="app">
       <header className="app-header">
@@ -13,15 +31,23 @@ function App() {
           <textarea
             className="text-editor"
             placeholder="Start writing here..."
+            value={currentChapter}
+            onChange={(e) => setCurrentChapter(e.target.value)}
           ></textarea>
+          <div className="button-group">
+            <button onClick={handleSaveChapter}>Save Chapter</button>
+            <button onClick={handleNewChapter}>New Chapter</button>
+          </div>
         </section>
-        <section className="app-section">
-          <h2>Edit and Format</h2>
-          {/* Add editing and formatting tools/components here */}
-        </section>
-        <section className="app-section">
-          <h2>Save and Export</h2>
-          {/* Add saving and exporting functionalities here */}
+        <section className="chapter-section">
+          <h3>Saved Chapters</h3>
+          <ul className="chapter-list">
+            {chapters.map((chapter, index) => (
+              <li key={index} onClick={() => handleChapterClick(chapter)}>
+                Chapter {index + 1}
+              </li>
+            ))}
+          </ul>
         </section>
       </main>
       <footer className="app-footer">
